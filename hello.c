54c4c0dccce48caab83466ce474d09e492cf9f94
AnthonyCandelas <tsallinger2@unl.edu>
2025-08-28 12:00:30 -0500

    added my major to the output

---
+++ hello.c
@@ -0,0 +1,19 @@
+/**
+ * Author: Anthony Candelas
+ *         tsallinger2@unl.edu
+ * Date: 2025/08/28
+ *
+ * Creating a simple line that prints my major in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Computer Science!\n");
+
+  return 0;
+}
+
+
